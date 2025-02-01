87eba4af5df0b573dc88238b608cc49a5f5ad50b
keal khondekar <kewalkhondekar25@gmail.com>
2025-02-01 22:46:55 +0530

    feat: let hoisting added

--- 01_Basics/04_hoisting/index.js
+++ 01_Basics/04_hoisting/index.js
@@ -3,3 +3,8 @@
 x = 10;
 document.write("x= " + x);
 var x;  //shadowing - use variable before declaring
+
+//let - will not allow hoisting
+y = 20;
+document.write("y= " + y);
+let y;  //Cannot access 'y' before initialization
