1e9964ce3488e7f006543fabddb901e04b6285f0
keal khondekar <kewalkhondekar25@gmail.com>
2025-02-01 22:30:19 +0530

    feat: block scope added

--- 01_Basics/02_varLetConst/index.js
+++ 01_Basics/02_varLetConst/index.js
@@ -8,7 +8,22 @@ function f1(){
     var y = 20;
   };
 
-  document.write("x= " + x + " " + "y= " + y);
+  document.write("x= " + x + " " + "y= " + y + "<br>");
 };
 
 f1();
+
+//let - block scope variable
+function f2(){
+  let a;  // function block scope
+  a = 10;
+
+  if(a == 10){
+    let b = 20; //block scope
+    document.write("inside block scope: " + "a= " + a + " " + "b= " + b);  // a & b are accessible here
+  };
+
+  document.write("a= " + a + "<br>" + "b= " + b); //b is only accessible inside block
+};
+
+f2();
