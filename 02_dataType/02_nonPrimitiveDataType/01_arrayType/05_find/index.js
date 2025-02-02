1830fcda5059369bb52ef50ddef7464ec22d7086
keal khondekar <kewalkhondekar25@gmail.com>
2025-02-02 20:22:13 +0530

    feat: array find method added

---
+++ 02_dataType/02_nonPrimitiveDataType/01_arrayType/05_find/index.js
@@ -0,0 +1,5 @@
+const arr1 = [1, 2, 3, 4, 5];
+const arr2 = ["mumbai", "delhi", "pune"];
+
+console.log(arr1.find(item => item > 3)); //4 - returns only 1st occurance
+console.log(arr2.find(item => item == "pune2"));
