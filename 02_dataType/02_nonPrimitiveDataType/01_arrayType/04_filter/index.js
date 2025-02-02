5fb40d976293c9fded8059f1dbf1fa42f22c16db
keal khondekar <kewalkhondekar25@gmail.com>
2025-02-02 20:23:14 +0530

    feat: array filter method added

--- 02_dataType/02_nonPrimitiveDataType/01_arrayType/04_filter/index.js
+++ 02_dataType/02_nonPrimitiveDataType/01_arrayType/04_filter/index.js
@@ -1,11 +1,13 @@
+///filter - return all elements that matches the condition
+
 const arr = [100, 200, 300, 400, 500];
 
 function expensive(val){
   return val >= 300;
 };
 
-console.log(arr.filter(expensive));
-console.log(arr.filter(item => item <= 300));
-console.log(arr.filter(item => item != 500));
+console.log(arr.filter(expensive)); //[ 300, 400, 500 ]
+console.log(arr.filter(item => item <= 300)); //[ 100, 200, 300 ]
+console.log(arr.filter(item => item != 500)); //[ 100, 200, 300, 400 ]
 
 
