694a10f6a75d6ce32eec33caa09ebb76c88f4fd0
keal khondekar <kewalkhondekar25@gmail.com>
2025-02-25 15:47:43 +0530

    feat: map method added

--- 05_map/index.js
+++ 05_map/index.js
@@ -3,3 +3,12 @@ const arr = [ 1, 2, 3];
 const multiplyBy2 = arr.map(item => item * 2);
 
 console.log(multiplyBy2);
+
+const obj = [
+  { name: "Alice", age: 25 },
+  { name: "Bob", age: 30 },
+  { name: "Charlie", age: 28 }
+];
+
+const objNames = obj.map(item => item.name);
+console.log(objNames);
