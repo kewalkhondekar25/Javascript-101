73a967f1260f34e2d49263fa69f5f8fdde42b228
keal khondekar <kewalkhondekar25@gmail.com>
2025-02-25 16:35:07 +0530

    feat: array chaining methods added

--- 05_map/index.js
+++ 05_map/index.js
@@ -1,7 +1,5 @@
 const arr = [ 1, 2, 3];
-
 const multiplyBy2 = arr.map(item => item * 2);
-
 console.log(multiplyBy2);
 
 const obj = [
@@ -9,6 +7,10 @@ const obj = [
   { name: "Bob", age: 30 },
   { name: "Charlie", age: 28 }
 ];
-
 const objNames = obj.map(item => item.name);
 console.log(objNames);
+
+//map chaining
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const result = numbers.filter(item => item % 2 === 0).map(item => item * 2).reduce((acc, curVal) => acc + curVal , 0);
+console.log(result);
