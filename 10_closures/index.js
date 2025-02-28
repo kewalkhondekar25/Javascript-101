c24c06911fd72ee79b8d7e8cfd5ccc06294de857
keal khondekar <kewalkhondekar25@gmail.com>
2025-02-28 19:08:59 +0530

    feat: closure output added

--- 10_closures/index.js
+++ 10_closures/index.js
@@ -23,3 +23,27 @@ const sum = (a) => {
   };
 };
 console.log(sum(1)(2)(3)(4));
+
+//output - 1
+let count = 1;
+const outside = () => {
+  if(true){
+    let count = 2;
+    console.log(count);
+  }
+  console.log(count);
+}
+outside();
+
+//output - 2
+//create a func
+
+const createBase = (num1) => {
+  return (num2) => {
+    return num1 + num2;
+  };
+};
+
+const addSix = createBase(6);
+console.log(addSix(10));
+
