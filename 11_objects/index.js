9ac942fb212ea2f1768abc7c4787bf6cc2e5b69e
keal khondekar <kewalkhondekar25@gmail.com>
2025-03-01 12:52:21 +0530

    feat: Objects method added

--- 11_objects/index.js
+++ 11_objects/index.js
@@ -2,8 +2,45 @@ const user1 = {
   name: "will",
   score: 0,
   increment: function(){
-    return user1.score++;
+    return user1.score + 1;
   }
 };
-
 console.log(user1.increment());
+
+//creating object
+const user2 = {};
+user2.name = "tim",
+user2.score = 3;
+user2.increment = function(){
+  return user2.score + 1;
+};
+console.log(user1, user2);
+
+//using js in built method
+const user3 = Object.create(null);
+user3.name = "Eva",
+user3.score = 10;
+user3.increment = function(){
+  return user3.score + 1;
+};
+console.log(user3);
+
+//Avoid DRY
+const createUser = (name, score) => {
+  
+  const user = Object.create(null);
+  
+  user.name = name;
+  user.score = score;
+  user.increment = function(){
+    return user.score + 1;
+  };
+  
+  return user;
+};
+
+const user4 = createUser("charles", 16);
+console.log(user4);
+
+
+
