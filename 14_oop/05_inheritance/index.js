816fac1c046e690dad38a0b2b4c22e05a6286311
Kewal Khondekar <kewalkhondekar25@gmail.com>
2025-03-07 22:55:49 +0530

    feat: classes inheritance added

--- 14_oop/05_inheritance/index.js
+++ 14_oop/05_inheritance/index.js
@@ -10,3 +10,34 @@ const teacher = {
 
 console.log(teacher.__proto__.name);
 Object.setPrototypeOf(teacher, user);//new syntax
+
+//class Inheritance
+class User {
+
+  constructor(userName, email, password){
+    this.userName = userName;
+    this.email = email;
+    this.password = password;
+  };
+
+  printOne(){
+    return `UserName: ${this.userName}`
+  };
+};
+
+class Teacher extends User {
+
+  constructor(userName, email, password, role){
+    super(userName, email, password);
+    this.role = role;
+  };
+
+  printTwo(){
+    return `Username: ${this.userName} email: ${this.email} role: ${this.role}`
+  };
+};
+
+const tommy = new Teacher("tommy", "tommy@gmail.com", "tommy@", "science teacher");
+console.log(tommy);
+console.log(tommy.printTwo());
+
